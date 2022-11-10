7b108fd1eaf668a614191fe72e7a9101a6bec6bb
SON BK <87347502+ngocson98@users.noreply.github.com>
2022-11-10 13:54:19 +0700

    Update heart_v1.py

--- heart_v1.py
+++ heart_v1.py
@@ -138,6 +138,7 @@ def draw(main: Tk, render_canvas: Canvas, render_heart: Heart, render_frame=0):
 if __name__ == '__main__':
     root = Tk()
     canvas = Canvas(root, bg='black', height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
+    root.title("I LOVE YOU")
     canvas.pack()
     heart = Heart()
     draw(root, canvas, heart)
